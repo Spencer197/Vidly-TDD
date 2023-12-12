2f5ccf12a83c3efce9b10d5298f5254d3f2c6d45
Spencer197 <spncr@protonmail.com>
2023-12-12 20:53:26 +0900

    Refactor validation logic in return.js & add validate.js

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,5 +1,5 @@
-
-module.exports = (validator) => {
+//This logic moved here from returns.js in Lesson 14.15.
+module.exports = (validator) => {//const validate was changed to module.exports
   return (req, res, next) => {
     const { error } = validator(req.body);  
     if (error) return res.status(400).send(error.details[0].message);
--- routes/returns.js
+++ routes/returns.js
@@ -1,11 +1,12 @@
-const Joi = require('joi');
-const validate = require('../middleware/validate');
+const Joi = require('joi');//Added in Lesson 14.15 to refactor Validation logic
+const validate = require('../middleware/validate');//Import validate module-Lesson 14.15
 const {Rental} = require('../models/rental');//Needed for '404 if no rental found'
 const {Movie} = require('../models/movie');//Import Movie class from movie module
 const auth = require('../middleware/auth');//Import auth middleware
 const express = require('express');
 const router = express.Router();
 
+//Borrowed from genres.js in Lesson14.15
 router.post('/', [auth, validate(validateReturn)], async (req, res) => {//auth passed as arg to POST()
   const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
@@ -23,11 +24,11 @@ router.post('/', [auth, validate(validateReturn)], async (req, res) => {//auth p
   return res.send(rental);//Return response
 });
 
-function validateReturn(req) {
-  const schema = {
+function validateReturn(req) {//This code block borrowed from genre.js & modified in Lesson 14.15
+  const schema = {            //genre.js-function validateGenre(genre)
     customerId: Joi.objectId().required(),
     movieId: Joi.objectId().required()
-  };
+  }
 
   return Joi.validate(req, schema);
 }
